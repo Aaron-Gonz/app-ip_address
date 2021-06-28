1aa5447f761f61df3703644a0315ad62071da011
VoIP-Dude <aaron.c.gonzalez@lumen.com>
2021-06-28 17:38:39 +0000

    Lab 4

--- main.js
+++ main.js
@@ -23,6 +23,13 @@ const path = require('path');
  */
 const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
+/**
+ * Calculate and return the first host IP address from a CIDR subnet.
+ * @param {string} cidrStr - The IPv4 subnet expressed
+ *                 in CIDR format.
+ * @param {callback} callback - A callback function.
+ * @returns {object} (firstIpAddress) - Object that returns IP strings for properties ipv4 & ipv6. 
+ */
 
 
 class IpAddress {
@@ -34,50 +41,39 @@ class IpAddress {
     // under Documentation -> Developer Guides -> Log Class Guide
     log.info('Starting the IpAddress product.');
   }
-  /**
- * Calculate and return the first host IP address from a CIDR subnet.
- * @param {string} cidrStr - The IPv4 subnet expressed
- *                 in CIDR format.
- * @param {callback} callback - A callback function.
- * @returns {string} ipObj - Returns IP strings for ipv4 & ipv6. 
- */
-  getFirstIpAddress(cidrStr, callback) {
+    getFirstIpAddress(cidrStr, callback) {
+        // Initialize return arguments for callback
+        let firstIpAddress = {
+            ipv4 = null,
+            ipv6: null
+        };
+        let callbackError = null;
 
-    // Initialize return arguments for callback
-    let firstIpAddress = null;
-    let firstIpv6Address = null
-    let callbackError = null;
+        // Instantiate an object from the imported class and assign the instance to variable cidr.
+        const cidr = new IPCIDR(cidrStr);
+        // Initialize options for the toArray() method.
+        // We want an offset of one and a limit of one.
+        // This returns an array with a single element, the first host address from the subnet.
+        const options = {
+            from: 1,
+            limit: 1
+        };
 
-    // Instantiate an object from the imported class and assign the instance to variable cidr.
-    const cidr = new IPCIDR(cidrStr);
-    // Initialize options for the toArray() method.
-    // We want an offset of one and a limit of one.
-    // This returns an array with a single element, the first host address from the subnet.
-    const options = {
-        from: 1,
-        limit: 1
-    };
-
-    // Use the object's isValid() method to verify the passed CIDR.
-    if (!cidr.isValid()) {
-        // If the passed CIDR is invalid, set an error message.
-        callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-    } else {
-        // If the passed CIDR is valid, call the object's toArray() method.
-        // Notice the destructering assignment syntax to get the value of the first array's element.
-        [firstIpAddress] = cidr.toArray(options);
-        firstIpv6Address = getIpv4MappedIpv6Address(firstIpAddress)
-    }
-    const ipObj = {
-        ipv4: firstIpAddress,
-        ipv6: firstIpv6Address
-    };
-
-    // Call the passed callback function.
-    // Node.js convention is to pass error data as the first argument to a callback.
-    // The IAP convention is to pass returned data as the first argument and error
-    // data as the second argument to the callback function.
-    return callback(ipObj, callbackError);
-    }
+        // Use the object's isValid() method to verify the passed CIDR.
+        if (!cidr.isValid()) {
+            // If the passed CIDR is invalid, set an error message.
+            callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
+        } else {
+            // If the passed CIDR is valid, call the object's toArray() method.
+            // Notice the destructering assignment syntax to get the value of the first array's element.
+            [firstIpAddress.ipv4] = cidr.toArray(options);
+            firstIpv6Address.ipv6 = getIpv4MappedIpv6Address(firstIpAddress.ipv4)
+        }
+        // Call the passed callback function.
+        // Node.js convention is to pass error data as the first argument to a callback.
+        // The IAP convention is to pass returned data as the first argument and error
+        // data as the second argument to the callback function.
+        return callback(firstIpAddress, callbackError);
+        }
 }
 module.exports = new IpAddress;
